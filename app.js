f811a1b20006de609ec7ff39fecee0ecc8c1f8c2
singtolee <liqiang0618@outlook.com>
2018-07-30 10:23:56 +0700

    ms translation works now, need test

--- app.js
+++ app.js
@@ -1,19 +1,98 @@
 var jsonfile = require('jsonfile');
-var translate = require('translate-google');
+const MYKEY = require('./mykey');
+//var translate = require('translate-google');
 var args = require('commander');
+var MsTranslator = require('mstranslator');
+var client = new MsTranslator({
+    api_key: MYKEY.key
+  }, true);
 
 args
 .version('0.0.1')
+.option('-k, --keyword [value]', 'keyword to set')
 .option('-i, --inputFilePath <path>', 'Input file path')
 .option('-o, --outputFilePath <path>', 'Output file path')
 .parse(process.argv);
 
 const inFile = args.inputFilePath;
 const ofile = args.outputFilePath;
-
+const kw = args.keyword;
+console.log(kw);
+//console.log(MYKEY.key);
 jsonfile.readFile(inFile,function(err,obj){
-    singtoTransName(obj);
+
+    var i;
+    for(i=0;i<obj.length;i++){
+        obj[i].detail = strtoarray(obj[i].detail);
+        obj[i].status = false;
+        obj[i].uw = 0.2;
+        obj[i].keyword = kw;
+    }
+
+    //mstrans(obj);
+
+    //singtoTransName(obj);
+    mymstran(obj);
 })
+
+async function mymstran(arr){
+    for(const item of arr){
+        item.thName = await callapi(item.name);
+        if(skutype(item.sku)){
+            for(const sku of item.sku){
+                sku.thLabel = await callapi(sku.label)
+                for(const val of sku.values){
+                    val.thDesc = await callapi(val.desc)
+                }
+            }
+        }
+    }
+    write2file(arr);
+}
+
+async function mstrans(arr){
+    for (const item of arr) {
+        await client.translate({text:item.name,from:'zh-cn',to:'th'},function(err,data){
+            if(err){
+                console.error(err)
+            }else {
+                item.thName = data;
+                console.log(item.name);
+                console.log(item.thName);
+            }
+            
+        });
+        if(skutype(item.sku)){
+            for(const sku of item.sku){
+                await client.translate({text:sku.label,from:'zh-cn',to:'th'},function(err,data){
+                    if(err){
+                        console.error(err)
+                    }else{
+                        sku.thLabel = data;
+                        onsole.log(sku.label);
+                        onsole.log(sku.thLabel);
+                    }
+                    
+                })
+
+                for(const val of sku.values){
+                    await client.translate({text:val.desc,from:'zh-cn',to:'th'},function(err,data){
+                        if(err){
+                            console.error(err)
+                        }else {
+                            val.thDesc = data;
+                            onsole.log(val.desc);
+                            console.log(val.thDesc);
+                        }
+                        
+                    })
+                }
+            }
+        }
+    }
+    write2file(arr);
+
+}
 
 async function singtoTransName(array){
     for (const item of array) {
@@ -49,6 +128,35 @@ function skutype(sku){
 
 function write2file(arr){
     jsonfile.writeFile(ofile,arr,{spaces: 2, EOL: '\r\n'}, function(err){
-        console.error(err)
+        console.error("ERROR is :" + err)
     })
+}
+
+function callapi(text) {
+    return new Promise(resolve=>{
+        client.translate({text:text,from:'zh-cn',to:'th'},function(err,data){
+            if(err){
+                console.error(err)
+                reject(err)
+            }else{
+                console.log(text)
+                console.log(data)
+                resolve(data)
+            }
+        })
+    })
+}
+
+function strtoarray(str){
+	var jpg = str.match(/\/\/\S*.jpg/g);
+	var png = str.match(/\/\/\S*.png/g);
+	if(jpg&&png){
+		return jpg.concat(png)
+	}else {
+		if(jpg){
+			return jpg;
+		}else {
+			return png;
+		}
+	}
 }
